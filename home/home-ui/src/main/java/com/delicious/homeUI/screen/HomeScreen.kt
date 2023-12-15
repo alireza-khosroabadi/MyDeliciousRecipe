601378a2e1e059a55361d7217a1e45e24358ae4c
alireza-khosroabadi <khosroabadi.alireza@gmail.com>
2023-12-16 00:07:56 +0330

    add image loader for banner in home

--- home/home-ui/src/main/java/com/delicious/homeUI/screen/HomeScreen.kt
+++ home/home-ui/src/main/java/com/delicious/homeUI/screen/HomeScreen.kt
@@ -7,20 +7,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.delicious.homeDomain.model.randomRecipe.RandomRecipe
 import com.delicious.homeUI.viewModel.HomeUiState
 import com.delicious.homeUI.viewModel.HomeViewModel
+import okhttp3.internal.notifyAll
 
 @Composable
 fun HomeScreen(
@@ -44,15 +52,23 @@ fun RandomRecipeList(modifier: Modifier = Modifier, data: List<RandomRecipe>) {
     val pagerState = rememberPagerState(pageCount = { data.size })
     HorizontalPager(
         modifier = modifier
-            .background(Color.LightGray)
             .fillMaxWidth()
-            .height(200.dp),
+            .height(250.dp),
         state = pagerState,
     ) { page ->
-        // Our page content
-        Text(
-            text = data[page].title,
-            modifier = Modifier.fillMaxWidth()
+        val item = data[page]
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(item.image)
+                .crossfade(true)
+                .build(),
+//            placeholder = painterResource(R.drawable.placeholder),
+            contentDescription = item.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .fillMaxSize()
         )
     }
 }
